f0c1f87f6b9168c423394bfe095394ab8b0e65c3
Somesh K Bhaskar <someshkumarbhaskar@gmail.com>
2025-04-06 17:37:11 +0530

    fix: update output filenames in nodemon and webpack configurations to use 'index.js'

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -10,13 +10,16 @@ const __dirname = dirname(__filename);
 export default {
   entry: './src/index.ts',
   target: 'node',
+  mode: 'development',
+  devtool: 'source-map',
+  performance: { hints: false },
   externals: [nodeExternals()],
   module: {
     rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }],
   },
   resolve: { extensions: ['.ts', '.js'] },
   output: {
-    filename: 'bundle.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
